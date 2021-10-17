eb065a144b6e8efa3789b179c30cd55d5921ef97
Sneakometer <sneakometer@gmail.com>
2021-10-17 02:45:30 +0200

    :tada: Initial commit

---
+++ HealthAndSafetySkip/Patches/HealthWarningFlowCoordinatorPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace HealthAndSafetySkip.Patches
+{
+    [HarmonyPatch(typeof(HealthWarningFlowCoordinator), "Update")]
+    internal class HealthWarningFlowCoordinatorPatch
+    {
+        internal static void Prefix(HealthWarningFlowCoordinator __instance)
+        {
+            __instance.GoToNextScene();
+        }
+    }
+}
---
+++ HealthAndSafetySkip/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace HealthAndSafetySkip
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static IPALogger Log { get; private set; }
+
+        private const string _harmonyId = "de.sneakometer.HealthAndSafetySkip";
+        internal static Harmony Harmony { get; private set; }
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Log = logger;
+            Harmony = new Harmony(_harmonyId);
+            Log.Info("HealthAndSafetySkip initialized.");
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        => Harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        [OnDisable]
+        public void OnDisable()
+            => Harmony.UnpatchAll(_harmonyId);
+    }
+}
